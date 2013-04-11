84ce0fbf7d1d405edcfe99fa3f40bf6e5b97e7b7
wesleymadrigal <wesley7879@gmail.com>
2013-04-10 19:56:39 -0500

    Update longest_repetition.py

--- longest_repetition.py
+++ longest_repetition.py
@@ -30,3 +30,24 @@ def longest_repetition(L):
                 elif e.isalpha():
                     keys_and_indexes[e] = L.index(e)
         return min([x for x,y in keys_and_indexes.items()])
+        
+        
+# a better version
+
+def longest_repetition(input_list):
+    best_element = None
+    length = 0
+    current = None
+    current_length = 0
+    for element in input_list:
+            if current != element:
+                    current = element
+                    current_length = 1
+            else:
+                    current_length = current_length + 1
+            if current_length > length:
+                    best_element = current
+                    length = current_length
+    return best_element
+    
+    
